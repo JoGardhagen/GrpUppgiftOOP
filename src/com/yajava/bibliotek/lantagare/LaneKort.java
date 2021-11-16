c75560bda2baec3fdb2f527eb1a7da38826f7725
JJspa <JJspa@DESKTOP-053R101>
2021-11-16 13:11:17 +0100

    Commit ny

--- src/com/yajava/bibliotek/lantagare/LaneKort.java
+++ src/com/yajava/bibliotek/lantagare/LaneKort.java
@@ -1,5 +1,18 @@
 package com.yajava.bibliotek.lantagare;
 
 public class LaneKort {
+	private int laneKortNr;
+	
+	public LaneKort(int laneKortNr) {
+		
+		this.setLaneKortNr(laneKortNr);
+	}
 
+	public int getLaneKortNr() {
+		return laneKortNr;
+	}
+
+	public void setLaneKortNr(int laneKortNr) {
+		this.laneKortNr = laneKortNr;
+	}
 }
--- src/com/yajava/bibliotek/lantagare/Lantagare.java
+++ src/com/yajava/bibliotek/lantagare/Lantagare.java
@@ -1,5 +1,15 @@
 package com.yajava.bibliotek.lantagare;
 
 public class Lantagare {
-
+	private int KortInfo;
+	
+	public int getKortInfo() {
+		return KortInfo;
+	}
+	public void setKortInfo(int kortInfo, int laneKortNr) {
+		KortInfo = laneKortNr;
+		
+	}
+	
+	
 }
