5dd1cf4b47fd0b398535dcd2a5d66a73745e3011
JJspa <JJspa@DESKTOP-053R101>
2021-11-16 13:01:38 +0100

    Commit nytt

--- src/com/grp3/grpupOOP_kod/Main.java
+++ src/com/grp3/grpupOOP_kod/Main.java
@@ -1,9 +1,13 @@
 package com.grp3.grpupOOP_kod;
 
+import com.yajava.bibliotek.bok.Bok;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Bok bok1 = new Bok("The Bible");
+		
+		System.out.println(bok1.getTitle());
 	}
 
 }
---
+++ src/com/yajava/bibliotek/bok/Bok.java
@@ -0,0 +1,19 @@
+package com.yajava.bibliotek.bok;
+
+public class Bok {
+	private String title;
+	
+	public Bok(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	
+}
